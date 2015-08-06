2be675dd14da967ff5eddd0f2b4b695041aa8339
JP Hastings-Spital <jphastings@gmail.com>
2015-08-06 09:02:08 +0100

    Combined rendering

--- js/animate.js
+++ js/animate.js
@@ -2,13 +2,21 @@ $(document).ready(function() {
   var intervals = {};
 
   function every(milliseconds, fn) {
-    setInterval(fn, milliseconds);
+    intervals[milliseconds] = intervals[milliseconds] || [];
+    intervals[milliseconds].push(fn);
+
+    if (intervals[milliseconds].length == 1) {
+      setInterval(function () {
+        for(var i = 0; i < intervals[milliseconds].length; ++i) {
+          intervals[milliseconds][i]();
+        }
+      }, milliseconds);
+    }
   }
 
   $('[data-sprite]').each(function(i, el) {
     el = $(el);
     var img = new Image();
-    img.frames = el.data('frames');
     img.onload = function() { startAnimation(el, img) };
     img.src = el.data('sprite');
   });
@@ -30,13 +38,22 @@ $(document).ready(function() {
 
   $('[data-pan]').each(function(i, el) {
     el = $(el);
-    var pps = parseInt(el.data('fps')) * parseInt(el.data('speed'));
-    every(1000 / pps, function() { forwardScroll(el) });
+    var img = new Image();
+    img.onload = function() { startScroll(el, img) };
+    img.src = el.data('pan');
   });
 
+  function startScroll(el, img) {
+    el.css({
+      'height': img.height,
+      'background': "url('" + img.src + "')",
+    });
+    el.imgWidth = img.width;
+    every(1000 / parseInt(el.data('fps')), function() { forwardScroll(el) });
+  };
+
   function forwardScroll(el) {
-    var tileWidth = parseInt(el.css('background-size').split('px')[0]);
-    el.pxOffset = ((el.pxOffset || 0) + 1) % tileWidth; // TODO: grab width of tile
-    el.css({ 'background-position': '-' + el.pxOffset + 'px 0' });
-  }
+    el.pxOffset = ((el.pxOffset || 0) + parseInt(el.data('speed'))) % el.imgWidth;
+    el.css({ 'background-position': '' + (-1 * el.pxOffset) + 'px 0' });
+  };
 })
